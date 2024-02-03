bb1985a60e1a58e7bd58e92de9a500099ce8ad91
prink <prinkaldoshi@gmail.com>
2024-02-03 16:40:57 +0530

    icons changed.

--- frontend/app/components/SideNav.jsx
+++ frontend/app/components/SideNav.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect } from 'react'
 import { MdDeleteOutline } from "react-icons/md";
-
+import { LuLayoutDashboard } from "react-icons/lu";
 export default function SideNav() {
   const path = usePathname()
   return (
@@ -16,10 +16,7 @@ export default function SideNav() {
       <ul className="space-y-2 text-sm font-medium">
         <li>
           <Link href="/home" className={`flex items-center rounded-lg px-3 py-2 text-slate-900 dark:text-white ${path.includes("home")?" bg-primary hover:bg-orange-600":"hover:bg-slate-100  dark:hover:bg-slate-700"} `}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" width="24" height="24" aria-hidden="true" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
-              <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-              <polyline points="9 22 9 12 15 12 15 22" />
-            </svg>
+          <LuLayoutDashboard className='h-5 w-5'   />
             <span className="ml-3 flex-1 whitespace-nowrap">Dashboard</span>
           </Link>
         </li>
--- frontend/app/home/page.jsx
+++ frontend/app/home/page.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { PiTextAlignRightFill } from "react-icons/pi";
+import { FaPlus } from "react-icons/fa6";
 
-export default function page() {
+export default function home() {
   return (
-    <div className=' flex min-h-screen py-12 px-12 flex-col '>
-        Home
+    <div className='flex flex-col py-10 px-10 '>
+
+      <div className='text-3xl font-bold'>Create your Video</div>
+      <div className='py-2 border-b w-full flex border-slate-700'></div>
+      <div className='flex flex-row gap-10 py-10'>
+        <Card>
+          <CardHeader>
+            <CardTitle>
+
+              <div className='flex flex-row justify-between items-center '>
+
+                <PiTextAlignRightFill className='h-7 w-7'/>
+                <FaPlus className='text-gray-500 h-4 w-4' />
+              </div>
+            </CardTitle>
+            <CardTitle>AI text to Video</CardTitle>
+            <CardDescription>
+              Generate short video clip from a single line of text.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+
+      </div>
     </div>
   )
 }
