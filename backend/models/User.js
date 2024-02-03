2db9ad7c8609e147482c6652b8c15e42af46043d
arnitdo <arnav.nitin.deo@gmail.com>
2024-02-03 13:01:48 +0530

    Update backend deps

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,24 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const userSchema = new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        unique:true,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    date:{
-        type:Date,
-        default:Date.now,
-        required:true
-    },
-  });
-  const User=mongoose.model('testuser',userSchema);
-  module.exports=User;
+
+});
+const User=mongoose.model('users',userSchema);
+module.exports=User;
