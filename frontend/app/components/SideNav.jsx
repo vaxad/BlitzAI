f49bb0c31542859d0897153818ab1192de4f1900
vaxad <varadprabhu111@gmail.com>
2024-02-03 14:42:19 +0530

    side nav

--- frontend/app/components/SideNav.jsx
+++ frontend/app/components/SideNav.jsx
@@ -1,7 +1,10 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 export default function SideNav() {
+  const path = usePathname()
   return (
   <aside id="sidebar" className=" -z-10 absolute left-0 -bottom-0 translate-y-full h-screen w-64 transition-transform" aria-label="Sidebar">
     <div className="flex h-full flex-col overflow-y-auto border-r border-slate-200 bg-white px-3 py-4 dark:border-slate-700 dark:bg-transparent">
@@ -11,7 +14,7 @@ export default function SideNav() {
       </div> */}
       <ul className="space-y-2 text-sm font-medium">
         <li>
-          <Link href="/home" className="flex items-center rounded-lg px-3 py-2 text-slate-900 hover:bg-slate-100 dark:text-white dark:hover:bg-slate-700">
+          <Link href="/home" className={`flex items-center rounded-lg px-3 py-2 text-slate-900 dark:text-white ${path.includes("home")?" bg-primary hover:bg-orange-600":"hover:bg-slate-100  dark:hover:bg-slate-700"} `}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" width="24" height="24" aria-hidden="true" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
               <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
               <polyline points="9 22 9 12 15 12 15 22" />
@@ -20,7 +23,7 @@ export default function SideNav() {
           </Link>
         </li>
         <li>
-          <Link href="/customers" className="flex items-center rounded-lg px-3 py-2 text-slate-900 hover:bg-slate-100 dark:text-white dark:hover:bg-slate-700">
+          <Link href="/customers" className={`flex items-center rounded-lg px-3 py-2 text-slate-900 dark:text-white ${path.includes("customers")?" bg-primary hover:bg-orange-600":"hover:bg-slate-100  dark:hover:bg-slate-700"} `}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" width="24" height="24" aria-hidden="true" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
               <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
               <circle cx="9" cy="7" r="4" />
@@ -31,7 +34,7 @@ export default function SideNav() {
           </Link>
         </li>
         <li>
-          <Link href="/products" className="flex items-center rounded-lg px-3 py-2 text-slate-900 hover:bg-slate-100 dark:text-white dark:hover:bg-slate-700">
+          <Link href="/products" className={`flex items-center rounded-lg px-3 py-2 text-slate-900 dark:text-white ${path.includes("products")?" bg-primary hover:bg-orange-600":"hover:bg-slate-100  dark:hover:bg-slate-700"} `}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" width="24" height="24" aria-hidden="true" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
               <path d="M16.5 9.4 7.55 4.24" />
               <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
@@ -42,7 +45,7 @@ export default function SideNav() {
           </Link>
         </li>
         <li>
-          <a href="#" className="flex items-center rounded-lg px-3 py-2 text-slate-900 hover:bg-slate-100 dark:text-white dark:hover:bg-slate-700">
+          <a href="#" className={`flex items-center rounded-lg px-3 py-2 text-slate-900 dark:text-white ${path.includes("settings")?" bg-primary hover:bg-orange-600":"hover:bg-slate-100  dark:hover:bg-slate-700"} `}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" width="24" height="24" aria-hidden="true" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
               <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
               <circle cx="12" cy="12" r="3" />
--- frontend/app/products/page.jsx
+++ frontend/app/products/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className=' flex flex-col p-12 bg-slate-100 dark:bg-slate-900 min-h-screen justify-center items-center'>
+    <div className=' flex flex-col p-12  min-h-screen justify-center items-center'>
         PRODUCTS
         <video src="http://res.cloudinary.com/db670bhmc/video/upload/v1706941505/fwxjac46q5ttnvd3rant.mp4" controls></video>
     </div>
