0a2fb94470f7cc514230556fc4c9b9cba2c1e05a
prink <prinkaldoshi@gmail.com>
2024-02-03 13:31:18 +0530

    login added.

---
+++ frontend/app/login/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login from '../components/Login'
+
+export default function login() {
+  return (
+    <div className='flex flex-col justify-center items-center min-h-[90vh]'>
+        <Login/>
+    </div>
+  )
+}
