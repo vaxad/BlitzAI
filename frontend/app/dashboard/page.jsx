e64baa70b7748a886ccd102bc737e4e17c1c0172
arnitdo <arnav.nitin.deo@gmail.com>
2024-02-04 07:26:03 +0530

    Finish dashboard

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -10,26 +10,87 @@ import {
 	SelectValue,
 } from "@/components/ui/select"
 import {CiBoxList, CiGrid42} from "react-icons/ci";
-import {IoMdImages} from "react-icons/io";
 import {FaHashtag} from "react-icons/fa";
-import {LuGalleryThumbnails} from "react-icons/lu";
-import {BiCaptions} from "react-icons/bi";
-import {MdDescription, MdOutlineKeyboardVoice} from "react-icons/md";
 import {Tabs, TabsList, TabsTrigger,} from "@/components/ui/tabs"
 import {Card, CardDescription, CardHeader, CardTitle,} from "@/components/ui/card"
 import {Input} from "@/components/ui/input"
+import Link from "next/link"
+
+const DASH_LINKS = [
+	{
+		href: "/script-to-title",
+		text: "Script to Title",
+		desc: "Generate a video script from a title"
+	},
+	{
+		href: "/script-to-description",
+		text: "Script to Description",
+		desc: "Generate a video script from a descriptive summary"
+	},
+	{
+		href: "/script-to-thumbnail",
+		text: "Script to Thumbnail",
+		desc: "Generate a video thumbnail from a script"
+	},
+	{
+		href: "/script-validation",
+		text: "Script Validation",
+		desc: "Ensure your content is safe-for-kids"
+	},
+	{
+		href: "/FaHashtag-to-hashtags",
+		text: "Script to Hashtags",
+		desc: "Generate SEO-friendly hash-tags"
+	},
+	{
+		href: "/title-to-thumbnail",
+		text: "Title to Thumbnail",
+		desc: "Generate a video thumbnail from a title"
+	},
+	{
+		href: "/title-to-description",
+		text: "Title to Description",
+		desc: "Generate a descriptive summary from a title"
+	},
+	{
+		href: "/video-to-hashtags",
+		text: "Video to Hashtags",
+		desc: "Generate SEO-friendly hash-tags from a video clip"
+	},
+	{
+		href: "/video-to-description",
+		text: "Video to Description",
+		desc: "Generate a descriptive summary from a video"
+	},
+	{
+		href: "/video-to-title",
+		text: "Video to Title",
+		desc: "Generate a title from a video"
+	},
+	{
+		href: "/text-to-image",
+		text: "Text to Image",
+		desc: "Generate an image from a prompt"
+	},
+	{
+		href: "/text-to-speech",
+		text: "Text to Speech",
+		desc: "Generate AI voice-overs for a paragraph"
+	}
+]
 
 export default function Home() {
 	const [tab, setTab] = useState("grid")
+	const [searchQuery, setSearchQuery] = useState("")
+
 	return (
 
 		<div className='flex flex-col gap-4'>
-			<div className='flex flex-col py-10 px-10 min-h-[200vh] '>
-
-
+			<div className='flex flex-col gap-8 py-10 px-10 min-h-[200vh] '>
 				<div className='flex  flex-row gap-5'>
-
-					<Input type="text" placeholder="Search" className="w-full"/>
+					<Input value={searchQuery} onChange={(e) => {
+						setSearchQuery(e.target.value)
+					}} type="text" placeholder="Search" className="w-full"/>
 					<Select defaultValue={"date"}>
 						<SelectTrigger className="w-[180px]">
 							<SelectValue placeholder="Sort By"/>
@@ -53,188 +114,61 @@ export default function Home() {
 						</TabsList>
 					</Tabs>
 				</div>
-				<Tabs>
-					{tab === "grid" ?
-						<div className='flex flex-row gap-10 py-4 w-full'>
-							<div>
-
-								<div className='flex flex-row gap-4 '>
-									<Card className="w-[350px]">
-										<CardHeader>
-											<CardDescription className=" flex justify-center">
-												<img src='/assets/title-to-hashtags.png' className='h-36'/>
-											</CardDescription>
-											<hr></hr>
-											<CardTitle className="flex text-primary pt-4 font-extrabold">Video To
-												Hashtag</CardTitle>
-											<CardDescription className="flex ">Create search friendly hashtags from the
-												video.</CardDescription>
-										</CardHeader>
-									</Card>
-
-									<Card className="w-[350px]">
-										<CardHeader>
-											<CardDescription className=" flex justify-center">
-												<img src='/assets/video-to-description.png' className='h-36'/>
-											</CardDescription>
-											<hr></hr>
-											<CardTitle className="flex text-primary pt-4 font-extrabold">Video To
-												Description</CardTitle>
-											<CardDescription className="flex ">Create search friendly description from
-												the video.</CardDescription>
-										</CardHeader>
-									</Card>
-
-									<Card className="w-[350px]">
-										<CardHeader>
-											<CardDescription className=" flex justify-center">
-												<img src='/assets/video-to-caption.png' className='h-36'/>
-											</CardDescription>
-											<hr></hr>
-											<CardTitle className="flex text-primary pt-4  font-extrabold">Video To
-												Caption</CardTitle>
-											<CardDescription className="flex ">Create search friendly captions from the
-												video.</CardDescription>
-										</CardHeader>
-									</Card>
-								</div>
-								<div className='flex flex-row gap-4 py-5'>
-									<Card className="w-[350px]">
-										<CardHeader>
-											<CardDescription className=" flex justify-center">
-												<img src='/assets/title-to-thumbmail.png' className='h-36'/>
-											</CardDescription>
-											<hr></hr>
-											<CardTitle className="flex text-primary pt-4  font-extrabold ">Title To
-												Thumbnail</CardTitle>
-											<CardDescription className="flex ">Create intuitive thumbnails from the
-												video titles.</CardDescription>
-										</CardHeader>
-									</Card>
-
-									<Card className="w-[350px]">
-										<CardHeader>
-											<CardDescription className=" flex justify-center">
-												<img src='/assets/title-to-voice.png' className='h-36'/>
-											</CardDescription>
-											<hr></hr>
-											<CardTitle className="flex text-primary pt-4  font-extrabold">Text To
-												Voice</CardTitle>
-											<CardDescription className="flex ">Create voice for videos from the video
-												scripts.</CardDescription>
-										</CardHeader>
-									</Card>
-
-									<Card className="w-[350px]">
-										<CardHeader>
-											<CardDescription className=" flex justify-center">
-												<img src='/assets/title-to-images.png' className='h-36'/>
-											</CardDescription>
-											<hr></hr>
-											<CardTitle className="flex text-primary pt-4  font-extrabold">Description To
-												Video</CardTitle>
-											<CardDescription className="flex ">Create videos from video
-												description.</CardDescription>
-										</CardHeader>
-									</Card>
-								</div>
-							</div>
-						</div> : <div className='flex flex-col gap-4'>
-							<div className='flex flex-col py-4 gap-4 '>
-								<Card className="w-full">
-									<CardHeader>
-										<div className='flex flex-row items-center '>
-											<FaHashtag className='h-8 w-8 text-white items-center'/>
-											<div className='flex flex-col'>
-
-												<CardTitle className="flex text-primary pt-2  font-extrabold px-4">Video
-													To Hashtag</CardTitle>
-												<CardDescription className="flex px-4">Create Search Friendly Hashtags
-													from the Video.</CardDescription>
-											</div>
-										</div>
-									</CardHeader>
-								</Card>
-
-
-								<Card className="w-full">
-									<CardHeader>
-										<div className='flex flex-row items-center '>
-											<MdDescription className='h-8 w-8 text-white items-center'/>
-											<div className='flex flex-col'>
-
-												<CardTitle className="flex text-primary pt-4  font-extrabold px-4">Video
-													To Description</CardTitle>
-												<CardDescription className="flex px-4">Create Search Friendly
-													Description from the Video.</CardDescription>
-											</div>
-										</div>
-									</CardHeader>
-								</Card>
-
-								<Card className="w-full">
-									<CardHeader>
-										<div className='flex flex-row items-center '>
-											<BiCaptions className='h-8 w-8 text-white items-center'/>
-											<div className='flex flex-col'>
-
-												<CardTitle className="flex text-primary pt-4  font-extrabold px-4">Video
-													To Caption</CardTitle>
-												<CardDescription className="flex px-4">Create Search Friendly Capt from
-													the Video.</CardDescription>
-											</div>
-										</div>
-									</CardHeader>
-								</Card>
-
-								<Card className="w-full">
-									<CardHeader>
-										<div className='flex flex-row items-center '>
-											<LuGalleryThumbnails className='h-8 w-8 text-white items-center'/>
-											<div className='flex flex-col'>
-
-												<CardTitle className="flex text-primary pt-2 px-4  font-extrabold">Title
-													To Thumbnail</CardTitle>
-												<CardDescription className="flex px-4">Create Search Friendly Hashtags
-													from the Video.</CardDescription>
-											</div>
-										</div>
-									</CardHeader>
-								</Card>
-
-
-								<Card className="w-full">
-									<CardHeader>
-										<div className='flex flex-row items-center '>
-											<MdOutlineKeyboardVoice className='h-8 w-8 text-white items-center'/>
-											<div className='flex flex-col'>
-
-												<CardTitle className="flex text-primary pt-4  font-extrabold px-4">Title
-													To Voice</CardTitle>
-												<CardDescription className="flex px-4">Create Search Friendly
-													Description from the Video.</CardDescription>
-											</div>
-										</div>
-									</CardHeader>
-								</Card>
-
-								<Card className="w-full">
-									<CardHeader>
-										<div className='flex flex-row items-center '>
-											<IoMdImages className='h-8 w-8 text-white items-center'/>
-											<div className='flex flex-col'>
-
-												<CardTitle className="flex text-primary pt-4  font-extrabold px-4">Time
-													To Images</CardTitle>
-												<CardDescription className="flex px-4">Create Search Friendly Capt from
-													the Video.</CardDescription>
-											</div>
-										</div>
-									</CardHeader>
-								</Card>
-							</div>
-						</div>}
-				</Tabs>
+				{tab === "grid" ? (
+						<div className={"w-full grid grid-cols-3 gap-8"}>
+							{DASH_LINKS.filter((dashObj) => {
+								return searchQuery === "" ||
+									dashObj.text.includes(searchQuery) ||
+									dashObj.desc.includes(searchQuery)
+							}).map((dashObj, dashIdx) => {
+								return (
+									<Link href={dashObj.href} key={dashObj.href}>
+										<Card className="w-[320px] h-[320px]">
+											<CardHeader>
+												<CardDescription className=" flex justify-center">
+													<img src={`/assets/dashsrc/${(dashIdx + 1) % 7}.png`}
+														 className='h-36'/>
+												</CardDescription>
+												<hr></hr>
+												<CardTitle
+													className="flex text-primary pt-4 font-extrabold">{dashObj.text}</CardTitle>
+												<CardDescription className="flex ">{dashObj.desc}</CardDescription>
+											</CardHeader>
+										</Card>
+									</Link>
+								)
+							})}
+						</div>
+					)
+					: (
+						<div
+							className='flex flex-col gap-8'
+						>
+							{DASH_LINKS.filter((dashObj) => {
+								return searchQuery === "" ||
+									dashObj.text.includes(searchQuery) ||
+									dashObj.desc.includes(searchQuery)
+							}).map((dashObj, dashIdx) => {
+								return (
+									<Link href={dashObj.href}>
+										<Card className="w-full">
+											<CardHeader>
+												<div className='flex flex-row items-center '>
+													<FaHashtag className='h-8 w-8 text-white items-center'/>
+													<div className='flex flex-col'>
+														<CardTitle
+															className="flex text-primary pt-2  font-extrabold px-4">{dashObj.text}</CardTitle>
+														<CardDescription
+															className="flex px-4">{dashObj.desc}</CardDescription>
+													</div>
+												</div>
+											</CardHeader>
+										</Card>
+									</Link>
+								)
+							})}
+						</div>
+					)}
 			</div>
 		</div>
 	)
